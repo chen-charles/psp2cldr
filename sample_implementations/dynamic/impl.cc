63853f19e76e4ec241a2a1dd98b4780d74cbf2a9
Jianye Chen <jianye.chen@uwaterloo.ca>
2021-05-11 22:15:43 -0400

    to be removed

--- sample_implementations/dynamic/impl.cc
+++ sample_implementations/dynamic/impl.cc
@@ -9,34 +9,6 @@
 
 #include <psp2cldr/imp_provider.hpp>
 
-DEFINE_VITA_IMP_NID_EXPORT(88758561, 391B74B7)
-{
-    DECLARE_VITA_IMP_TYPE(FUNCTION);
-    std::cout << "ksceDebugPrintf: entry" << std::endl;
-
-    uint32_t r0 = ctx->thread[RegisterAccessProxy::Register::R0]->r();
-    return ctx->handler_call_target_function("strlen", r0)
-        ->then([=](uint32_t result, InterruptContext *ctx) {
-            std::cout << "ksceDebugPrintf: strlen result=" << result << std::endl;
-            return ctx->handler_call_target_function("printf", r0)->then([=](uint32_t result, InterruptContext *ctx) {
-                std::cout << "ksceDebugPrintf: printf result=" << result << std::endl;
-
-                // flush
-                ctx->coord.proxy().w<uint8_t>(r0, '\n');
-                ctx->coord.proxy().w<uint8_t>(r0 + 1, 0);
-                return ctx->handler_call_target_function("printf", r0);
-            });
-        })
-        ->then([=](uint32_t result, InterruptContext *ctx) {
-            std::cout << "ksceDebugPrintf: printf(\"\\n\") result=" << result << std::endl;
-            ctx->thread[RegisterAccessProxy::Register::PC]->w(ctx->thread[RegisterAccessProxy::Register::LR]->r());
-            std::cout << "ksceDebugPrintf: exit" << std::endl;
-            return std::make_shared<HandlerResult>(0);
-        });
-
-    // return ksceDebugPrintf(ctx);
-}
-
 #undef _fstat // WATCHOUT: _fstat might be defined somewhere else
 DEFINE_VITA_IMP_SYM_EXPORT(_fstat)
 {
@@ -44,25 +16,9 @@ DEFINE_VITA_IMP_SYM_EXPORT(_fstat)
     uint32_t fd = ctx->thread[RegisterAccessProxy::Register::R0]->r();
     uint32_t buf = ctx->thread[RegisterAccessProxy::Register::R1]->r();
 
-    return ctx->handler_call_target_function("memset", buf, 0xcc, 0x58)->then([=](uint32_t result, InterruptContext *ctx) {
-        uint8_t st[0x58];
-        ctx->coord.proxy().copy_out(&st, buf, 0x58);
-        for (int i = 0; i < 0x58; i++)
-        {
-            if (st[i] != 0xcc)
-            {
-                return std::make_shared<HandlerResult>(1);
-            }
-        }
-
-        memset(&st, 0, sizeof(st));
-        *(uint32_t *)(&st[4]) = 0020000; // st_mode = S_IFCHR;
-        ctx->coord.proxy().copy_in(buf, &st, sizeof(st));
-
-        ctx->thread[RegisterAccessProxy::Register::R0]->w(0);
-        ctx->thread[RegisterAccessProxy::Register::PC]->w(ctx->thread[RegisterAccessProxy::Register::LR]->r());
-        return std::make_shared<HandlerResult>(0);
-    });
+    ctx->thread[RegisterAccessProxy::Register::R0]->w(-1);
+    ctx->thread[RegisterAccessProxy::Register::PC]->w(ctx->thread[RegisterAccessProxy::Register::LR]->r());
+    return std::make_shared<HandlerResult>(0);
 }
 
 DEFINE_VITA_IMP_SYM_EXPORT(_sbrk)
@@ -98,7 +54,11 @@ DEFINE_VITA_IMP_SYM_EXPORT(_close)
 DEFINE_VITA_IMP_SYM_EXPORT(_isatty)
 {
     DECLARE_VITA_IMP_TYPE(FUNCTION);
-    ctx->thread[RegisterAccessProxy::Register::R0]->w(1);
+    uint32_t fd = PARAM_0;
+    if (fd >= 0 && fd <= 2)
+        ctx->thread[RegisterAccessProxy::Register::R0]->w(1);
+    else
+        ctx->thread[RegisterAccessProxy::Register::R0]->w(0);
     ctx->thread[RegisterAccessProxy::Register::PC]->w(ctx->thread[RegisterAccessProxy::Register::LR]->r());
     return std::make_shared<HandlerResult>(0);
 }
@@ -114,18 +74,33 @@ DEFINE_VITA_IMP_SYM_EXPORT(_exit)
 DEFINE_VITA_IMP_SYM_EXPORT(_write)
 {
     DECLARE_VITA_IMP_TYPE(FUNCTION);
-    uint32_t ptr = ctx->thread[RegisterAccessProxy::Register::R1]->r();
-    uint32_t len = ctx->thread[RegisterAccessProxy::Register::R2]->r();
+    uint32_t fd = PARAM_0;
+    uint32_t ptr = PARAM_1;
+    uint32_t len = PARAM_2;
 
-    std::stringstream ss;
-    for (uint32_t i = 0; i < len; i++)
-        ss << ctx->coord.proxy().r<char>(ptr++);
+    char buf[len + 1];
+    memset(buf, 0, len + 1);
+    ctx->coord.proxy().copy_out(buf, ptr, len);
 
-    std::cout << "_write() called: " << ss.str();
+    switch (fd)
+    {
+    case 0:
+        // ?
+        std::cout << "_write() called on stdin: ";
+        break;
+    case 1:
+        std::cout << "stdout: ";
+        break;
+    case 2:
+        std::cout << "stderr: ";
+        break;
+    default:
+        throw;
+    }
+    std::cout << buf;
 
-    ctx->thread[RegisterAccessProxy::Register::R0]->w(len);
-    ctx->thread[RegisterAccessProxy::Register::PC]->w(ctx->thread[RegisterAccessProxy::Register::LR]->r());
-    return std::make_shared<HandlerResult>(0);
+    TARGET_RETURN(len);
+    HANDLER_RETURN(0);
 }
 
 #undef _open
