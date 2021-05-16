8ae9b6306c16a2ba41a84726ca223569412034f5
Jianye Chen <jianye.chen@uwaterloo.ca>
2021-05-16 00:40:02 -0400

    coordinator: TLS
    Signed-off-by: Jianye Chen <jianye.chen@uwaterloo.ca>

--- include/psp2cldr/coordinator.hpp
+++ include/psp2cldr/coordinator.hpp
@@ -6,6 +6,9 @@
 #include <exception>
 #include <functional>
 #include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <unordered_set>
 
 #include <psp2cldr/access_proxy.hpp>
 
@@ -13,6 +16,80 @@ class InterruptContext;
 class LoadContext;
 
 class ExecutionCoordinator;
+
+class TLSKey
+{
+public:
+    TLSKey() {}
+    ~TLSKey() { mapping.clear(); }
+
+    std::mutex lock;
+    std::unordered_map<uint32_t, uintptr_t> mapping; // threadID, value
+};
+
+class TLS
+{
+    static inline std::unordered_set<TLSKey *> tls;
+    static inline std::recursive_mutex tls_mutex;
+
+public:
+    TLS(uint32_t threadID) : m_id(threadID) {}
+
+    ~TLS()
+    {
+        std::lock_guard guard{tls_mutex};
+        for (auto &key : tls)
+            free((uintptr_t)key);
+    }
+
+    uintptr_t alloc()
+    {
+        std::lock_guard guard{tls_mutex};
+        auto key = new TLSKey();
+        tls.insert(key);
+        return (uintptr_t)key;
+    }
+
+    void set(uintptr_t key, uintptr_t value)
+    {
+        std::lock_guard guard{tls_mutex};
+        TLSKey *entry = (TLSKey *)key;
+        if (tls.count(entry))
+        {
+            std::lock_guard g{entry->lock};
+            entry->mapping[m_id] = value;
+        }
+        else
+            throw std::out_of_range("invalid key");
+    }
+
+    uintptr_t get(uintptr_t key) const
+    {
+        std::lock_guard guard{tls_mutex};
+        TLSKey *entry = (TLSKey *)key;
+        if (tls.count(entry))
+        {
+            std::lock_guard g{entry->lock};
+            return entry->mapping[m_id];
+        }
+        else
+            throw std::out_of_range("invalid key");
+    }
+
+    void free(uintptr_t key)
+    {
+        std::lock_guard guard{tls_mutex};
+        TLSKey *entry = (TLSKey *)key;
+        if (tls.erase(entry))
+            delete entry;
+        else
+            throw std::out_of_range("invalid key");
+    }
+
+protected:
+    uint32_t m_id;
+};
+
 class ExecutionThread
 {
 private:
@@ -28,7 +105,7 @@ public:
     }
 
 public:
-    ExecutionThread() {}
+    ExecutionThread() : tls(tid()) {}
     virtual ~ExecutionThread() {}
 
 public:
@@ -60,6 +137,9 @@ public:
     virtual void panic(int code = 0, LoadContext *load = nullptr) = 0;
 
     virtual void register_interrupt_callback(std::function<void(ExecutionCoordinator &, ExecutionThread &, uint32_t)> callback) = 0;
+
+public:
+    TLS tls;
 
 public:
     // register access
